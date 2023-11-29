1b74176bc6714801a49eedc90f0226ff7fcab261
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-29 22:03:50 +0100

    Init useQuery

---
+++ client/src/components/navigation/nav-layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { BottomNav, BottomNavItem } from 'flowbite-svelte';
+	import { CloseSolid, HomeSolid, PlusSolid } from 'flowbite-svelte-icons';
+	import { I18nNamespace } from '../../types/i18n/i18n-namespace.enum';
+	import { i18nGetContext } from '../../i18n/i18n-context';
+	import { clearAllCookies } from '../../utils/cookies/clear-all-cookies';
+
+	const i18n = i18nGetContext();
+
+	const logout = () => {
+		clearAllCookies();
+		window.location.reload();
+	};
+</script>
+
+<div class="container mx-auto">
+	<slot />
+
+	<BottomNav position="absolute" classInner="grid-cols-3">
+		<BottomNavItem btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Home`)}>
+			<HomeSolid
+				class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
+			/>
+		</BottomNavItem>
+		<BottomNavItem btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Register`)}>
+			<PlusSolid
+				class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
+			/>
+		</BottomNavItem>
+		<BottomNavItem
+			on:click={logout}
+			btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Exit`)}
+		>
+			<CloseSolid
+				class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
+			/>
+		</BottomNavItem>
+	</BottomNav>
+</div>
--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,57 +1,29 @@
 <script lang="ts">
 	import { setContext } from 'svelte';
 	import getI18nStore from '../i18n/setup-i18n';
-	import { BottomNav, BottomNavItem } from 'flowbite-svelte';
-	import { CloseSolid, HomeSolid, PlusSolid } from 'flowbite-svelte-icons';
-	import { I18nNamespace } from '../types/i18n/i18n-namespace.enum';
-	import '../app.pcss';
 	import Authenticate from '../components/auth/authenticate.svelte';
-	import { clearAllCookies } from '../utils/cookies/clear-all-cookies';
+	import NavLayout from '../components/navigation/nav-layout.svelte';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import '../app.pcss';
 
 	const isAuthenticated = false;
 
 	const i18n = getI18nStore();
 	setContext('i18n', i18n);
 
-	const logout = () => {
-		clearAllCookies();
-		window.location.reload();
-	};
+	const queryClient = new QueryClient();
 </script>
 
 <div>
-	{#if isAuthenticated}
-		<div class="container mx-auto">
-			<slot />
-
-			<BottomNav position="absolute" classInner="grid-cols-3">
-				<BottomNavItem btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Home`)}>
-					<HomeSolid
-						class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
-					/>
-				</BottomNavItem>
-				<BottomNavItem
-					btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Register`)}
-				>
-					<PlusSolid
-						class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
-					/>
-				</BottomNavItem>
-				<BottomNavItem
-					on:click={logout}
-					btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Exit`)}
-				>
-					<CloseSolid
-						class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
-					/>
-				</BottomNavItem>
-			</BottomNav>
-		</div>
-	{:else}
-		<div class="flex justify-center align-center w-screen h-screen">
-			<div class="flex flex-col align-center justify-center"><Authenticate /></div>
-		</div>
-	{/if}
+	<QueryClientProvider client={queryClient}>
+		{#if isAuthenticated}
+			<NavLayout />
+		{:else}
+			<div class="flex justify-center align-center w-screen h-screen">
+				<div class="flex flex-col align-center justify-center"><Authenticate /></div>
+			</div>
+		{/if}</QueryClientProvider
+	>
 </div>
 
 <style>
