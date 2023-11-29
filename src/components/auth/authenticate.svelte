bb2bf0682f99bfbf7ae17218c895509bf2390f94
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-29 13:36:25 +0100

    Basic state management

--- src/components/auth/authenticate.svelte
+++ src/components/auth/authenticate.svelte
@@ -5,15 +5,29 @@
 	import { LockOpenSolid } from 'flowbite-svelte-icons';
 
 	const i18n = i18nGetContext();
+
+	$: passwordState = '';
+
+	const onValueChange = (e: EventTarget | null) => {
+		passwordState = (e as HTMLInputElement)?.value ?? '';
+	};
+
+	const onLogin = () => {};
 </script>
 
 <div class="cont grid grid-cols-1 gap-3">
 	<form>
-		<FloatingLabelInput style="outlined" id="password" name="password" type="password">
+		<FloatingLabelInput
+			style="outlined"
+			id="password"
+			name="password"
+			type="password"
+			on:change={(e) => onValueChange(e.target)}
+		>
 			{$i18n.t(`${I18nNamespace.AUTH}.form.Password`)}
 		</FloatingLabelInput>
 	</form>
-	<Button ico class="login"
+	<Button ico class="login" on:click={onLogin}
 		><LockOpenSolid class="w-3.5 h-3.5 mr-2" />{$i18n.t(`${I18nNamespace.AUTH}.form.Login`)}</Button
 	>
 </div>
