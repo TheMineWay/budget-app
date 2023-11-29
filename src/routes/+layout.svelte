6833936edbca145a33e09d3ebcb3094d9acec9f7
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-29 13:48:00 +0100

    Logout logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,12 @@
 	import { setContext } from 'svelte';
 	import getI18nStore from '../i18n/setup-i18n';
 	import { BottomNav, BottomNavItem } from 'flowbite-svelte';
-	import { HomeSolid, PlusSolid } from 'flowbite-svelte-icons';
+	import { CloseSolid, HomeSolid, PlusSolid } from 'flowbite-svelte-icons';
 	import { I18nNamespace } from '../types/i18n/i18n-namespace.enum';
 	import { page } from '$app/stores';
 	import '../app.pcss';
 	import Authenticate from '../components/auth/authenticate.svelte';
+	import { clearAllCookies } from '../utils/cookies/clear-all-cookies';
 
 	const i18n = getI18nStore();
 	setContext('i18n', i18n);
@@ -17,6 +18,11 @@
 		}
 	} = $page;
 	const isAuthenticated: boolean = _isAuthenticated;
+
+	const logout = () => {
+		clearAllCookies();
+		window.location.reload();
+	};
 </script>
 
 <div>
@@ -24,7 +30,7 @@
 		<div class="container mx-auto">
 			<slot />
 
-			<BottomNav position="absolute" classInner="grid-cols-2">
+			<BottomNav position="absolute" classInner="grid-cols-3">
 				<BottomNavItem btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Home`)}>
 					<HomeSolid
 						class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
@@ -34,6 +40,14 @@
 					btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Register`)}
 				>
 					<PlusSolid
+						class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
+					/>
+				</BottomNavItem>
+				<BottomNavItem
+					on:click={logout}
+					btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Exit`)}
+				>
+					<CloseSolid
 						class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
 					/>
 				</BottomNavItem>
