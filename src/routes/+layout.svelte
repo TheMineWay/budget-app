e66948b5e0626e0bd1874e7ef4e85b2a3ac9e4e0
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-29 11:54:59 +0100

    Add add btn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import { setContext } from 'svelte';
 	import getI18nStore from '../i18n/setup-i18n';
 	import { BottomNav, BottomNavItem } from 'flowbite-svelte';
-	import { HomeSolid } from 'flowbite-svelte-icons';
+	import { HomeSolid, PlusSolid } from 'flowbite-svelte-icons';
 	import { I18nNamespace } from '../types/i18n/i18n-namespace.enum';
 	import '../app.pcss';
 
@@ -13,9 +13,14 @@
 <div>
 	<slot />
 
-	<BottomNav position="absolute" classInner="grid-cols-4">
+	<BottomNav position="absolute" classInner="grid-cols-2">
 		<BottomNavItem btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Home`)}>
 			<HomeSolid
+				class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
+			/>
+		</BottomNavItem>
+		<BottomNavItem btnName={$i18n.t(`${I18nNamespace.COMMON}.layout.bottom-navigation.Register`)}>
+			<PlusSolid
 				class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
 			/>
 		</BottomNavItem>
