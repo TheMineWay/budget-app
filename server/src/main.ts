9cf4f95d9a66b687d7efbafde1c7bd16df4292db
Joel Campos <joelcamposoliva9000@gmail.com>
2023-12-01 00:18:28 +0100

    Remove HTTPS options by now

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions:
-      process.env.USE_HTTPS !== 'false'
-        ? {
-            key: fs.readFileSync('/app/certificates/key.pem'),
-            cert: fs.readFileSync('/app/certificates/cert.pem'),
-          }
-        : {},
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
