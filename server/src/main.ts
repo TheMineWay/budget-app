4668e4c08015768b2e3fc462a271574cdd75c771
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-30 16:53:59 +0100

    API certs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions:
+      process.env.USE_HTTPS !== 'false'
+        ? {
+            key: fs.readFileSync('/app/certificates/key.pem'),
+            cert: fs.readFileSync('/app/certificates/cert.pem'),
+          }
+        : {},
+  });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors();
