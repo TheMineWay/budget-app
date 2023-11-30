4e9dd7ece30e7e82092d7e54b0fc0639a5cb0462
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-30 16:47:24 +0100

    Add filter

--- client/src/components/transactions/list/transactions-list.svelte
+++ client/src/components/transactions/list/transactions-list.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
-	import { Pagination, Timeline } from 'flowbite-svelte';
+	import { Input, Pagination, Timeline } from 'flowbite-svelte';
 	import { transactionsQuery } from '../../../queries/transactions/transactions.query';
 	import { categoriesQuery } from '../../../queries/categories/categories.query';
 	import Transaction from './transaction.svelte';
 	import { i18nGetContext } from '../../../i18n/i18n-context';
 	import { I18nNamespace } from '../../../types/i18n/i18n-namespace.enum';
+	import { SearchSolid } from 'flowbite-svelte-icons';
 
 	const i18n = i18nGetContext();
 
 	$: limit = 10;
 	$: offset = 0;
+	$: text = '';
 
-	$: transactionsList = transactionsQuery({ limit, offset });
+	$: transactionsList = transactionsQuery({
+		limit,
+		offset,
+		text: text.trim() === '' ? undefined : text.trim()
+	});
 	$: total = $transactionsList.data?.count ?? 0;
 
 	const categories = categoriesQuery();
@@ -25,11 +31,22 @@
 		if (newOffset < 0) newOffset = 0;
 		offset = newOffset;
 	};
+
+	const onFilterChange = (e: EventTarget | null) => {
+		text = (e as HTMLInputElement).value;
+	};
 </script>
 
 <div class="h-screen">
+	<div style="width: 100%;" class="flex justify-center">
+		<div class="mb-6">
+			<Input on:change={(e) => onFilterChange(e.target)} id="filter" type="text" placeholder="...">
+				<SearchSolid slot="left" class="w-5 h-5 text-gray-500 dark:text-gray-400" />
+			</Input>
+		</div>
+	</div>
 	<div class="flex justify-center">
-		<Timeline order="vertical" class="">
+		<Timeline order="vertical">
 			{#each rows as row}
 				<Transaction
 					categoryName={$categories.data?.data.find((c) => c.id === row.categoryId)?.name ?? '...'}
